39732a24e1f5264912b57432a3608ba9bdef1784
yuliana <yuli.mejiap@gmail.com>
2025-04-09 19:21:16 -0500

    se crean los endpoints para el controlador librocontrolador

--- appLibraryBackend/src/main/java/com/example/appLibraryBackend/controladores/LibroControlador.java
+++ appLibraryBackend/src/main/java/com/example/appLibraryBackend/controladores/LibroControlador.java
@@ -1,13 +1,29 @@
 package com.example.appLibraryBackend.controladores;
 
+import com.example.appLibraryBackend.modelos.Libro;
+import com.example.appLibraryBackend.servicios.LibroServicio;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/libros")
 public class LibroControlador {
     //Inyectar el servicio de libro(libroservicio)
     @Autowired
-    
+    private LibroServicio libroServicio;
+
+    //Utilizar los EndPoints
+    //Endpoint Post libros
+    @PostMapping
+    public Libro addLibro(@RequestBody Libro libro){
+        return libroServicio.saveLibro(libro);
+    }
+
+    //Enpoint de todos los libros
+    @GetMapping
+    public List<Libro> getLibrosAll(){
+        return libroServicio.getAllLibros();
+    }
 }
